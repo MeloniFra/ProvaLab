1a58f8cf6731f9e90c0f539d267dccc83005486c
MeloniFra <francesco.meloni.stud@ispascalcomandini.it>
2021-11-12 09:35:11 +0100

    Aggiunta Interfaccia e MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,56 @@ namespace EsercizioBiblioteca
         public MainWindow()
         {
             InitializeComponent();
+            rbt_titolo.IsChecked = true;
+            lbl_nLibri = biblioteca.GetNLibri;
+        }
+
+        Biblioteca biblioteca;
+
+        private void btn_cerca_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (rbt_titolo.IsChecked == true)
+                {
+                    foreach (Libro l in biblioteca.GetLibriStessoTitolo(tbx_cerca.Text))
+                    {
+                        lbx_libri.Items.Add(l);
+                    }
+                }
+                else
+                {
+                    foreach (Libro l in biblioteca.GetLibriStessoAutore(tbx_cerca.Text))
+                    {
+                        lbx_libri.Items.Add(l);
+                    }
+                }
+                tbx_cerca.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex.Message);
+            }
+        }
+
+        private void btn_aggiungiLibro_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Libro libro = new Libro(tbx_autore.Text, tbx_titolo.Text, int.Parse(tbx_annoDiPubblicazione.Text), tbx_editore.Text, int.Parse(tbx_nPagine.Text));
+                biblioteca.AggiungiLibro(Libro);
+                tbx_autore.Text = "";
+                tbx_titolo.Text = "";
+                tbx_annoDiPubblicazione.Text = "";
+                tbx_editore.Text = "";
+                tbx_nPagine.Text = "";
+                lbl_nLibri = biblioteca.GetNLibri;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("" + ex.Message);
+            }
         }
     }
 }
